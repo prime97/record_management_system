a8d8542eaf9700a03089a11c1d048957fe0ce531
Mark Shillaw <116576050+edu-mryo@users.noreply.github.com>
2025-03-14 06:48:38 +0900

    Button/ButtonGroup Material integration
    Adding Material UI package to utilize modern ButtonUI

--- src/frontend/react-records/src/App.js
+++ src/frontend/react-records/src/App.js
@@ -1,9 +1,16 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import React from "react";
+import Button from '@mui/material/Button';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Clients from "./Clients";
 import Airlines from "./Airlines";
 import Flights from "./Flights";
 import './App.css';
+import { ButtonGroup } from "@mui/material";
 
 function App() {
   return (
@@ -12,9 +19,11 @@ function App() {
         <h2>Record Management System</h2>
         <nav>
           <ul>
-            <li><Link to="/clients">Clients</Link></li>
-            <li><Link to="/airlines">Airlines</Link></li>
-            <li><Link to="/flights">Flights</Link></li>
+            <ButtonGroup id='btn_group' variant="contained" aria-label="Basic button group">
+              <Button><Link to="/clients">Clients</Link></Button>
+              <Button><Link to="/airlines">Airlines</Link></Button>
+              <Button><Link to="/flights">Flights</Link></Button>
+            </ButtonGroup>
           </ul>
         </nav>
         <Routes>
