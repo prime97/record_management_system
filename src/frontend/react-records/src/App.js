b50e715bcf3d3bf911b603ea3a1478802a574d31
Mark Shillaw <116576050+edu-mryo@users.noreply.github.com>
2025-03-18 08:23:55 +0900

    adding search bar on top of the UI
    Adding a nonfunctional search bar on top of the UI.

--- src/frontend/react-records/src/App.js
+++ src/frontend/react-records/src/App.js
@@ -3,28 +3,44 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import React from "react";
+import {React,useState} from "react";
 import Button from '@mui/material/Button';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Clients from "./Clients";
 import Airlines from "./Airlines";
 import Flights from "./Flights";
 import './App.css';
-import { ButtonGroup,Paper ,Stack} from "@mui/material";
+import { TextField,Paper ,Stack} from "@mui/material";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState(""); // State for the search query
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <Router>
       {/* Wrap the main content in a Paper component */}
       <Paper elevation={20} sx={{ padding: '20px', maxWidth: '500px',  margin: '20px auto' ,marginTop:'50px'}}>
         <div id='main'>
           <h1>Record Management System</h1>
+          <TextField
+            variant="outlined"
+            label="Search"
+            size="small"
+            placeholder="Search Records..."
+            value={searchQuery}
+            fullWidth
+            onChange={handleSearchChange}
+            margin="normal" // Add some spacing around the TextField
+          />
+
           <nav>
             {/* <ul> */}
               {/* <ButtonGroup id='btn_group' variant="contained" aria-label="Basic button group"> */}
                 {/* Use Link components *inside* the Buttons */}
                 <Stack direction="row" spacing={2}>
-
                 <Button variant='contained'component={Link} to="/clients">Clients</Button>
                 <Button variant='contained'component={Link} to="/airlines">Airlines</Button>
                 <Button variant='contained'component={Link} to="/flights">Flights</Button>
