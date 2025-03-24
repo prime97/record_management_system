942e07789723db6e5667cdce9295d12160d1c3d9
Nferry02 <sgnferry@liverpool.ac.uk>
2025-03-24 21:46:58 +0100

    Updated Clients tests

--- src/frontend/react-records/src/Clients.test.js
+++ src/frontend/react-records/src/Clients.test.js
@@ -18,12 +18,34 @@ describe("Clients Component", () => {
       render(<Clients />);
     });
 
+    fireEvent.change(screen.getByPlaceholderText("Search by Client name"), {
+      target: { value: "J Doe" }
+    });
+
     await waitFor(() => {
-      expect(screen.getByText("J Doe - New York")).toBeInTheDocument();
+      expect(screen.getByText("J Doe - 12345678")).toBeInTheDocument();
     });
 
     // Ensure axios.get is called with the correct URL
     expect(axios.get).toHaveBeenCalledWith("http://localhost:5000/clients");
+  });
+
+  test("search filters clients correctly", async () => {
+    axios.get.mockResolvedValueOnce({ data: [
+      { id: 1, name: "J Doe", address_line_1: "Place", city: "New York", state: "New York", zip_code: "NY123", country: "America", phone_number: "12345678" },
+      { id: 2, name: "Joan Smith", address_line_1: "New Place", city: "Los Angeles", state: "California", zip_code: "CA123", country: "America", phone_number: "87654321" }
+    ]});
+
+    await act(async () => {
+      render(<Clients />);
+    });
+
+    fireEvent.change(screen.getByPlaceholderText("Search by Client name"), {
+      target: { value: "Joan" }
+    });
+
+    expect(screen.queryByText("J Doe - 12345678")).not.toBeInTheDocument();
+    expect(screen.getByText("Joan Smith - 87654321")).toBeInTheDocument();
   });
 
   test("adds a new client", async () => {
@@ -35,14 +57,14 @@ describe("Clients Component", () => {
     });
 
     const Clientbutton = screen.getByText(/Clients/i);
-    const nameInput = screen.getByPlaceholderText(/Name/i);
+    const nameInput = screen.getByPlaceholderText(/Name/);
     const address_line_1Input = screen.getByPlaceholderText(/Address Line 1/i);
     const cityInput = screen.getByPlaceholderText(/City/i);
     const stateInput = screen.getByPlaceholderText(/State/i);
     const zip_codeInput = screen.getByPlaceholderText(/Zip Code/i);
     const countryInput = screen.getByPlaceholderText(/Country/i);
     const phone_numberInput = screen.getByPlaceholderText(/Phone Number/i);       
-    const addButton = screen.getByText(/Add Client/i);
+    const addButton = screen.getByText("Add");
 
     // Triggering changes and click events
     fireEvent.click(Clientbutton);
@@ -60,8 +82,12 @@ describe("Clients Component", () => {
       axios.get.mockResolvedValueOnce({ data: [{ id: 1, name: "Jane Smith", address_line_1: "Place", city: "New York", state: "New York", zip_code: "NY123", country: "America", phone_number: "12345678" }] });
     });
 
+    fireEvent.change(screen.getByPlaceholderText("Search by Client name"), {
+      target: { value: "Jane" }
+    });
+
     await waitFor(() => {
-      expect(screen.getByText("Jane Smith - New York")).toBeInTheDocument();
+      expect(screen.getByText("Jane Smith - 12345678")).toBeInTheDocument();
     });
 
     // Ensure axios.post was called correctly
@@ -72,8 +98,12 @@ describe("Clients Component", () => {
     axios.get.mockResolvedValue({ data: [{ id: 3, name: "Jordan Simmons", address_line_1: "34 New Place", city: "Dallas", state: "Texas", zip_code: "TX123", country: "America", phone_number: "987654321" }] });
     axios.put.mockResolvedValue({});
     render(<Clients />);
+
+    fireEvent.change(screen.getByPlaceholderText("Search by Client name"), {
+      target: { value: "Jordan" }
+    });
   
-    await waitFor(() => expect(screen.getByText("Jordan Simmons - Dallas")).toBeInTheDocument());
+    await waitFor(() => expect(screen.getByText("Jordan Simmons - 987654321")).toBeInTheDocument());
     fireEvent.click(screen.getByText("Edit"));
     fireEvent.change(screen.getByPlaceholderText("Name"), { target: { value: "Winona Davies" } });
       
@@ -92,8 +122,12 @@ describe("Clients Component", () => {
       render(<Clients />);
     });
 
+    fireEvent.change(screen.getByPlaceholderText("Search by Client name"), {
+      target: { value: "John" }
+    });
+
     await waitFor(() => {
-      expect(screen.getByText("John Doe - New York")).toBeInTheDocument();
+      expect(screen.getByText("John Doe - 12345678")).toBeInTheDocument();
     });
 
     const deleteButton = screen.getByText(/DELETE/i);
@@ -104,6 +138,10 @@ describe("Clients Component", () => {
       axios.get.mockResolvedValueOnce({ data: [] });
     });
 
+    fireEvent.change(screen.getByPlaceholderText("Search by Client name"), {
+      target: { value: "John" }
+    });
+    
     await waitFor(() => {
       expect(screen.queryByText("John Doe - New York")).not.toBeInTheDocument();
     });
@@ -115,7 +153,7 @@ describe("Clients Component", () => {
   test("renders Clients component correctly", () => {
     render(<Clients />);
     expect(screen.getByText(/Clients/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(/Name/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Name/)).toBeInTheDocument();
     expect(screen.getByText(/Add Client/i)).toBeInTheDocument();
   });
 });
